84bde8e832a82444b97b41bbbb82801a2e7f5948
Niraj Rao <niraj.a.rao@berkeley.edu>
2021-07-18 18:01:59 -0700

    Refactor registers to use 8 bit values

--- src/chip.rs
+++ src/chip.rs
@@ -11,10 +11,10 @@ pub struct Chip8 {
     pc: usize, // Program Counter
     sp: usize, // Stack Pointer
     I: u16,    // Index Register
-    registers: [u16; NUM_REGISTERS],
+    V: [u8; NUM_REGISTERS],
     keys: [u8; NUM_KEYS],
-    delay_timer: u16,
-    sound_timer: u16,
+    delay_timer: u8,
+    sound_timer: u8,
     graphics: [[u8; 64]; 32],
 }
 
@@ -25,7 +25,7 @@ impl Chip8 {
             memory_buffer: memory_buffer,
             stack: [0; STACK_LEVELS],
             keys: [0; NUM_KEYS],
-            registers: [0; NUM_REGISTERS],
+            V: [0; NUM_REGISTERS],
             graphics: [[0; 64]; 32],
             pc: 0x200,
             sp: 0,
@@ -66,18 +66,18 @@ impl Chip8 {
             }
             // 3XNN - Skip next instruction if Vx == NN
             0x3000 => {
-                let value = opcode & 0x00FF;
+                let value = (opcode & 0x00FF) as u8;
                 let register_number = opcode & 0x0F00;
-                if self.registers[register_number as usize] == value {
+                if self.V[register_number as usize] == value {
                     self.pc += 2;
                 }
                 self.pc += 2;
             }
             // 4XNN - Skip next instruction if Vx != NN
             0x4000 => {
-                let value = opcode & 0x00FF;
+                let value = (opcode & 0x00FF) as u8;
                 let register_number = opcode & 0x0F00;
-                if self.registers[register_number as usize] != value {
+                if self.V[register_number as usize] != value {
                     self.pc += 2;
                 }
                 self.pc += 2;
@@ -86,7 +86,7 @@ impl Chip8 {
             0x5000 => {
                 let register_x = opcode & 0x0F00;
                 let register_y = opcode & 0x00F0;
-                if self.registers[register_x as usize] == self.registers[register_y as usize] {
+                if self.V[register_x as usize] == self.V[register_y as usize] {
                     self.pc += 2;
                 }
                 self.pc += 2;
@@ -94,15 +94,15 @@ impl Chip8 {
             // 6XNN - Sets Vx to NN
             0x6000 => {
                 let register_x = opcode & 0x0F00;
-                let value = opcode & 0x00FF;
-                self.registers[register_x as usize] = value;
+                let value = (opcode & 0x00FF) as u8;
+                self.V[register_x as usize] = value;
                 self.pc += 2;
             }
             // 7XNN - Adds N to Vx
             0x7000 => {
                 let register_x = opcode & 0x0F00;
-                let value = opcode & 0x00FF;
-                self.registers[register_x as usize] += value;
+                let value = (opcode & 0x00FF) as u8;
+                self.V[register_x as usize] += value;
                 self.pc += 2;
             }
             0x8000 => {
@@ -111,31 +111,31 @@ impl Chip8 {
                     0x0000 => {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
-                        self.registers[register_x as usize] = self.registers[register_y as usize];
+                        self.V[register_x as usize] = self.V[register_y as usize];
                         self.pc += 2;
                     }
                     // 8XY1 - Set Vx to Vx or Vy
                     0x001 => {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
-                        self.registers[register_x as usize] = self.registers[register_x as usize]
-                            | self.registers[register_y as usize];
+                        self.V[register_x as usize] =
+                            self.V[register_x as usize] | self.V[register_y as usize];
                         self.pc += 2;
                     }
                     // 8XY2 - Set Vx to Vx & Vy
                     0x002 => {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
-                        self.registers[register_x as usize] = self.registers[register_x as usize]
-                            & self.registers[register_y as usize];
+                        self.V[register_x as usize] =
+                            self.V[register_x as usize] & self.V[register_y as usize];
                         self.pc += 2;
                     }
                     // 8XY3 - Set Vx to Vx xor (^) Vy
                     0x003 => {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
-                        self.registers[register_x as usize] = self.registers[register_x as usize]
-                            ^ self.registers[register_y as usize];
+                        self.V[register_x as usize] =
+                            self.V[register_x as usize] ^ self.V[register_y as usize];
                         self.pc += 2;
                     }
                     // 8XY4 - Add Vy to Vx. VF is set to 0 when there's a borrow, and 1 when there
@@ -143,14 +143,13 @@ impl Chip8 {
                     0x004 => {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
-                        let sum = self.registers[register_x as usize]
-                            + self.registers[register_y as usize];
+                        let sum = self.V[register_x as usize] + self.V[register_y as usize];
 
-                        if sum > 0xFF {
-                            self.registers[15] = 1;
+                        if sum > 0xFF as u8 {
+                            self.V[15] = 1;
                         }
 
-                        self.registers[register_x as usize] = sum;
+                        self.V[register_x as usize] = sum;
                         self.pc += 2;
                     }
                     // Vy is subtracted from Vx. Vf is set to 0 when there's a borrow and 1 when
@@ -159,20 +158,18 @@ impl Chip8 {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
 
-                        let difference = self.registers[register_x as usize]
-                            - self.registers[register_y as usize];
+                        let difference = self.V[register_x as usize] - self.V[register_y as usize];
 
                         // TODO: VF is set to 0 when there's a borrow, and 1 when there is not.
 
-                        self.registers[register_x as usize] = difference;
+                        self.V[register_x as usize] = difference;
                         self.pc += 2;
                     }
                     // Stores the least significant bit of Vx in Vf and shits Vx to the right by 1.
                     0x006 => {
                         let register_x = opcode & 0x0F00;
-                        self.registers[15] = self.registers[register_x as usize] & 0b00000001;
-                        self.registers[register_x as usize] =
-                            self.registers[register_x as usize] >> 1;
+                        self.V[15] = self.V[register_x as usize] & 0b00000001;
+                        self.V[register_x as usize] = self.V[register_x as usize] >> 1;
                         self.pc += 2;
                     }
                     // Sets Vx to Vy minus Vx. Vf is set to 0 when there's a borrow and 1 when
@@ -181,20 +178,18 @@ impl Chip8 {
                         let register_x = opcode & 0x0F00;
                         let register_y = opcode & 0x00F0;
 
-                        let difference = self.registers[register_y as usize]
-                            - self.registers[register_x as usize];
+                        let difference = self.V[register_y as usize] - self.V[register_x as usize];
 
                         // TODO: VF is set to 0 when there's a borrow, and 1 when there is not.
 
-                        self.registers[register_x as usize] = difference;
+                        self.V[register_x as usize] = difference;
                         self.pc += 2;
                     }
                     // Stores the most significant bit of Vx in Vf and shits Vx to the left by 1.
                     0x00E => {
                         let register_x = opcode & 0x0F00;
-                        self.registers[15] = self.registers[register_x as usize] & 0b10000000;
-                        self.registers[register_x as usize] =
-                            self.registers[register_x as usize] << 1;
+                        self.V[15] = self.V[register_x as usize] & 0b10000000;
+                        self.V[register_x as usize] = self.V[register_x as usize] << 1;
                         self.pc += 2;
                     }
                     _ => println!("No Match"),
@@ -204,7 +199,7 @@ impl Chip8 {
             0x9000 => {
                 let register_x = opcode & 0x0F00;
                 let register_y = opcode & 0x00F0;
-                if self.registers[register_x as usize] != self.registers[register_y as usize] {
+                if self.V[register_x as usize] != self.V[register_y as usize] {
                     self.pc += 2;
                 }
                 self.pc += 2;
@@ -217,27 +212,27 @@ impl Chip8 {
             }
             // Jumps to the address NNN plus V0.
             0xB000 => {
-                let address = opcode & 0x0FFF;
-                self.pc = (self.registers[0] + address) as usize;
+                let address = (opcode & 0x0FFF) as u8;
+                self.pc = (self.V[0] + address) as usize;
             }
             // Sets Vx to the result of a bitwise and operation on a random number (0-255).
             0xC000 => {
-                let value = opcode & 0x00FF;
+                let value = (opcode & 0x00FF) as u8;
                 let register_x = opcode & 0x0F00;
-                self.registers[register_x as usize] = (rand::random::<u8>() & value as u8) as u16;
+                self.V[register_x as usize] = rand::random::<u8>() & value;
                 self.pc += 2;
             }
             0xE000 => match opcode & 0x00FF {
                 0x009E => {
                     let register_x = opcode & 0x0F00;
-                    if self.keys[self.registers[register_x as usize] as usize] == 1 {
+                    if self.keys[self.V[register_x as usize] as usize] == 1 {
                         self.pc += 2;
                     }
                     self.pc += 2;
                 }
                 0x00A1 => {
                     let register_x = opcode & 0x0F00;
-                    if self.keys[self.registers[register_x as usize] as usize] == 0 {
+                    if self.keys[self.V[register_x as usize] as usize] == 0 {
                         self.pc += 2;
                     }
                     self.pc += 2;
@@ -247,7 +242,7 @@ impl Chip8 {
             0xF000 => match opcode & 0x00FF {
                 0x0007 => {
                     let register_x = opcode & 0x0F00;
-                    self.registers[register_x as usize] = self.delay_timer;
+                    self.V[register_x as usize] = self.delay_timer;
                     self.pc += 2;
                 }
                 0x000A => {
@@ -255,34 +250,33 @@ impl Chip8 {
                     for item in self.keys.iter().enumerate() {
                         let (idx, value): (usize, &u8) = item;
                         if *value == 1 {
-                            self.registers[register_x as usize] = idx as u16;
+                            self.V[register_x as usize] = idx as u8;
                             self.pc += 2;
                         }
                     }
                 }
                 0x0015 => {
                     let register_x = opcode & 0x0F00;
-                    self.delay_timer = self.registers[register_x as usize];
+                    self.delay_timer = self.V[register_x as usize];
                     self.pc += 2;
                 }
                 0x0018 => {
                     let register_x = opcode & 0x0F00;
-                    self.sound_timer = self.registers[register_x as usize];
+                    self.sound_timer = self.V[register_x as usize];
                     self.pc += 2;
                 }
                 0x001E => {
                     let register_x = opcode & 0x0F00;
-                    self.I += self.registers[register_x as usize];
+                    self.I += self.V[register_x as usize] as u16;
                     self.pc += 2;
                 }
                 0x0055 => {
                     let mut offset = 0;
                     let x = opcode & 0x0F00;
                     for idx in 0..=x {
-                        let value = self.registers[idx as usize];
-                        self.memory_buffer[(self.I + offset) as usize] = (value & 0xFF00) as u8;
-                        self.memory_buffer[(self.I + offset + 1) as usize] = (value & 0x00FF) as u8;
-                        offset += 2;
+                        let value = self.V[idx as usize];
+                        self.memory_buffer[(self.I + offset) as usize] = value;
+                        offset += 1;
                     }
                     self.pc += 2;
                 }
@@ -290,10 +284,8 @@ impl Chip8 {
                     let x = opcode & 0x0F00;
                     let mut offset = 0;
                     for idx in 0..=x {
-                        self.registers[idx as usize] =
-                            (self.memory_buffer[(self.I + offset) as usize] as u16) << 8
-                                | (self.memory_buffer[(self.I + offset + 1) as usize]) as u16;
-                        offset += 2;
+                        self.V[idx as usize] = self.memory_buffer[(self.I + offset) as usize];
+                        offset += 1;
                     }
                     self.pc += 2;
                 }
